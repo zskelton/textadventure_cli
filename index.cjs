1a92ed17fc517ecce845344c15956bf900c19ecb
Zachary D. Skelton <zbert@hotmail.com>
2022-01-07 16:08:28 -0600

    Initial commit.

---
+++ index.cjs
@@ -0,0 +1,18 @@
+var term = require('terminal-kit').terminal;
+
+term.clear();
+term.cyan("You are in a forrest with a cave entrance directly to the east.");
+term.cyan("You see a note on the ground.");
+
+var items = [
+    'Go',
+    'Get',
+    'Help'
+];
+
+term.singleColumnMenu(items, (error, response) => {
+    term('\n').eraseLineAfter.green(
+        `#${response.selectedIndex} selected: ${response.selectedText} (${response.x}, ${response.y}})`
+    );
+    process.exit();
+});
