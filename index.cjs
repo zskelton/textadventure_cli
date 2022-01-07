a7695c3f98e6cf3ef9f4aa960d9b8591cfdce7a2
Zachary D. Skelton <zbert@hotmail.com>
2022-01-07 16:22:15 -0600

    build(layout): create import/export of main

--- index.cjs
+++ index.cjs
@@ -1,18 +1,3 @@
-var term = require('terminal-kit').terminal;
+const main = require('./app/index.cjs');
 
-term.clear();
-term.cyan("You are in a forrest with a cave entrance directly to the east.");
-term.cyan("You see a note on the ground.");
-
-var items = [
-    'Go',
-    'Get',
-    'Help'
-];
-
-term.singleColumnMenu(items, (error, response) => {
-    term('\n').eraseLineAfter.green(
-        `#${response.selectedIndex} selected: ${response.selectedText} (${response.x}, ${response.y}})`
-    );
-    process.exit();
-});
+main();
